abb6b9ca956ea5b678beb4b456194567e9ecc066
NastyaMix <kravchenkonastik@gmail.com>
2023-04-16 17:18:48 +0300

    feat: add mac address solution

--- src/mac-address.js
+++ src/mac-address.js
@@ -4,20 +4,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * The MAC-48 address is six groups of two hexadecimal digits (0 to 9 or A to F),
  * separated by hyphens.
  *
- * Your task is to check by given string inputString
+ * Your task is to check by given ning inputning
  * whether it's a MAC-48 address or not.
  *
- * @param {Number} inputString
+ * @param {Number} inputning
  * @return {Boolean}
  *
  * @example
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function isMAC48Address(n) {
+  const reg = n.match(/[0-9A-F][0-9A-F]/gi).join('-');
+  return reg.length < n.length ? false : true;
 }
+
 module.exports = {
   isMAC48Address
 };
